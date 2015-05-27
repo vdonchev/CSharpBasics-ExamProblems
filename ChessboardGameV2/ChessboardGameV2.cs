2d70889f7a619b185550254e8256dd1cb449c3b8
xclusivePc <xmt@abv.bg>
2015-05-27 18:21:03 +0300

    update

---
+++ ChessboardGameV2/ChessboardGameV2.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+class ChessboardGameV2
+{
+    static void Main()
+    {
+
+    }
+}
---
+++ KingOfThievesV2/KingOfThieves.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class KingOfThieves
+{
+    static void Main()
+    {
+        int rows = int.Parse(Console.ReadLine());
+        char innerSymbol = char.Parse(Console.ReadLine());
+
+        int innerCount = 1;
+        int outterCount = rows / 2;
+
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        for (int i = 0; i < rows; i++)
+        {
+            Console.WriteLine("{0}{1}{0}", new string('-', outterCount), new string(innerSymbol, innerCount));
+            if(i < rows / 2 )
+            {
+                innerCount += 2;
+                outterCount--;
+            }
+            else
+            {
+                innerCount -= 2;
+                outterCount++;
+            }
+        }
+    }
+}
