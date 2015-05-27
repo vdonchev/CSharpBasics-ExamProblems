36912ada44076728d370ae6a6faffd7977879031
vdonchev <ya.5164646777752015@yandex.ru>
2015-05-27 14:40:03 +0300

    solved 100/100

--- BookProblem/BookProblem.cs
+++ BookProblem/BookProblem.cs
@@ -1,16 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace BookProblem
+class BookProblem
 {
-    class BookProblem
+    static void Main()
     {
-        static void Main(string[] args)
-        {
+        // Inputs
+        int bookPages = int.Parse(Console.ReadLine());
+        int campingDays = int.Parse(Console.ReadLine());
+        int readPages = int.Parse(Console.ReadLine());
 
+        // Logic
+        int readingDays = 30 - campingDays;
+        int readPerMonth = readingDays * readPages;
+
+        if (readPerMonth <= 0)
+        {
+            Console.WriteLine("never");
+        }
+        else
+        {
+            double monthsNeeded = Math.Ceiling((double)bookPages / readPerMonth);
+            int years = (int)monthsNeeded / 12;
+            double months = (double)monthsNeeded % 12;
+
+            // Output
+            Console.WriteLine("{0} years {1} months", (int)years, (int)months);
         }
     }
 }
