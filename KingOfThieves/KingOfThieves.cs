fd4028b7f3506edf5eb04124b54df0fb75724250
xclusivePc <xmt@abv.bg>
2015-05-27 16:33:23 +0300

    Solved 100/100

---
+++ KingOfThieves/KingOfThieves.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class KingOfThieves
+{
+    static void Main()
+    {
+        // inputs
+        int size = int.Parse(Console.ReadLine());
+        char type = char.Parse(Console.ReadLine());
+
+        // top part
+        for (int i = 0; i < size / 2 + 1; i++)
+        {
+            int gemStr = 1 + (i * 2);
+            int spaceStr = (size - gemStr) / 2;
+            string gem = new string(type, gemStr);
+            string space = new string('-', spaceStr);
+            Console.WriteLine("{0}{1}{0}", space, gem);
+        }
+        // bottom part
+        for (int i = size / 2; i > 0; i--)
+        {
+            int gemStr = (i * 2) - 1;
+            int spaceStr = (size - gemStr) / 2;
+            string gem = new string(type, gemStr);
+            string space = new string('-', spaceStr);
+            Console.WriteLine("{0}{1}{0}", space, gem);
+        }
+    }
+}
