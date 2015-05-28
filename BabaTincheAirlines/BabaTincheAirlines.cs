47a4f8d1b8a4d6dbaba1b6c94a9f696fa482ac22
xclusivePc <xmt@abv.bg>
2015-05-28 18:39:22 +0300

    update

---
+++ BabaTincheAirlines/BabaTincheAirlines.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class BabaTincheAirlines
+{
+    static void Main()
+    {
+        // inputs
+        string[] firstClass = Console.ReadLine().Split();
+        string[] secondClass = Console.ReadLine().Split();
+        string[] thirdClass = Console.ReadLine().Split();
+
+        // calculate
+        int firstClassFreqIncome = (int)Convert.ToInt32(firstClass[1]) * (int)(7000 * 0.3);
+        int firstClassRegIncome = (int)(Convert.ToInt32(firstClass[0]) - Convert.ToInt32(firstClass[1])) * 7000;
+        int firstClassIncome = (int)Convert.ToInt32(firstClass[2]) * (int)(0.005 * 7000);
+        int totalIncomeFirstClass = firstClassFreqIncome + firstClassRegIncome + firstClassIncome;
+
+        int secondClassFreqIncome = (int)Convert.ToInt32(secondClass[1]) * (int)(3500 * 0.3);
+        int secondClassRegIncome = (int)(Convert.ToInt32(secondClass[0]) - Convert.ToInt32(secondClass[1])) * 3500;
+        int secondClassIncome = (int)Convert.ToInt32(secondClass[2]) * (int)(0.005 * 3500);
+        int totalIncomeSecondClass = secondClassFreqIncome + secondClassRegIncome + secondClassIncome;
+
+        int thirdClassFreqIncome = (int)Convert.ToInt32(thirdClass[1]) * (int)(1000 * 0.3);
+        int thirdClassRegIncome = (int)(Convert.ToInt32(thirdClass[0]) - Convert.ToInt32(thirdClass[1])) * 1000;
+        int thirdClassIncome = (int)Convert.ToInt32(thirdClass[2]) * (int)(0.005 * 1000);
+        int totalIncomeThirdClass = thirdClassFreqIncome + thirdClassRegIncome + thirdClassIncome;
+
+        int allIncome = totalIncomeFirstClass + totalIncomeSecondClass + totalIncomeThirdClass;
+        Console.WriteLine(allIncome);
+    }
+}
---
+++ Budget/Budget.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Budget
+{
+    static void Main()
+    {
+        // inputs
+        int kirosMoney = int.Parse(Console.ReadLine());
+        int daysOut = int.Parse(Console.ReadLine());
+        int homeWeekends = int.Parse(Console.ReadLine());
+
+        // logic
+        int normalWeekends = (4 - homeWeekends) * 40;
+        int normalDays = (22 - daysOut) * 10;
+        int goOutDays = (int)(0.03 * kirosMoney) + 10;
+        int spentMoney = 150 + normalWeekends + normalDays + (daysOut * goOutDays);
+
+        // output
+        if (spentMoney == kirosMoney) 
+        {
+            Console.WriteLine("Exact Budget.");
+        }
+        else if (spentMoney > kirosMoney)
+        {
+            Console.WriteLine("No, not enough {0}.", spentMoney - kirosMoney);
+        }
+        else
+        {
+            Console.WriteLine("Yes, leftover {0}.", kirosMoney - spentMoney);
+        }
+    }
+}
