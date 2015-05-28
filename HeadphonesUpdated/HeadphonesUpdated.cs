7bd0a5938928de1e3bffdb91dc8ea7fb2d6d2825
xclusivePc <xmt@abv.bg>
2015-05-28 08:30:05 +0300

    update

---
+++ HeadphonesUpdated/HeadphonesUpdated.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class HeadphonesUpdated
+{
+    static void Main()
+    {
+        // inputs
+        int diameter = 9;
+
+        //print top
+        for (int i = 0; i < diameter; i++)
+        {
+            char bg = '-';
+            if(i == 0)
+            {
+                bg = '*';
+            }
+            Console.WriteLine("{0}*{1}*{0}", new string('-', diameter / 2), new string(bg, diameter));
+        }
+
+        // print phones
+        int backCount = diameter / 2;
+        int foreCount = 1;
+        for (int i = 0; i < diameter; i++)
+        {
+            Console.WriteLine("{0}{1}{0}-{0}{1}{0}", new string('-', backCount), new string('*', foreCount));
+            if(i < diameter / 2)
+            {
+                backCount--;
+                foreCount += 2;
+            }
+            else
+            {
+                backCount++;
+                foreCount -= 2;
+            }
+        }
+    }
+
+}
---
+++ PetarsGame/PetarsGame.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class PetarsGame
+{
+    static void Main()
+    {
+        //inputs
+        ulong startNumber = ulong.Parse(Console.ReadLine());
+        ulong endNumber = ulong.Parse(Console.ReadLine());
+        string replacement = Console.ReadLine();
+
+        ulong totalSum = 0;
+        for (ulong i = startNumber; i < endNumber; i++)
+        {
+            if(i % 5 == 0)
+            {
+                totalSum += i;
+            }
+            else
+            {
+                totalSum += i % 5;
+            }
+        }
+
+        string a = totalSum.ToString();
+        string forReplace;
+
+        if(totalSum % 2 != 0)
+        {
+            forReplace = a[a.Length - 1].ToString();
+        }
+        else
+        {
+            forReplace = a[0].ToString();
+        }
+
+        //Console.WriteLine(totalSum);
+        string b = a.Replace(forReplace, replacement);
+        Console.WriteLine(b);
+    }
+}
