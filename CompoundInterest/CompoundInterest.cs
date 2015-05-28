7bbadd17b4546905d144f3cc2faf3974f2479d14
xclusivePc <xmt@abv.bg>
2015-05-28 07:37:03 +0300

    update

---
+++ CompoundInterest/CompoundInterest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class CompoundInterest
+{
+    static void Main()
+    {
+        // inputs
+        decimal tvPrice = decimal.Parse(Console.ReadLine());
+        int bankTerm = int.Parse(Console.ReadLine());
+        decimal bankInt = decimal.Parse(Console.ReadLine());
+        decimal friendInt = decimal.Parse(Console.ReadLine());
+
+        decimal bankLoan = calculateLoan(tvPrice, bankInt, bankTerm);
+        decimal friendLoan = calculateLoan(tvPrice, friendInt, 1);
+        string cheapLoan = (bankLoan < friendLoan) ? "Bank" : "Friend";
+        decimal loanSize = Math.Min(bankLoan, friendLoan);
+        Console.WriteLine("{0:F2} {1}", loanSize, cheapLoan);
+    }
+
+    private static decimal calculateLoan(decimal tvPrice, decimal interest, int term)
+    {
+        decimal readyInt = (1 + interest);
+        decimal outputInt = 1;
+        for (int i = 0; i < term; i++)
+        {
+            outputInt *= readyInt;
+        }
+        return tvPrice * outputInt;
+    }
+}
