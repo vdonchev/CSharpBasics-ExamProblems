6dba1873473e4c7c83d4289beee2a348c6d7283c
xclusivePc <xmt@abv.bg>
2015-06-02 14:44:21 +0300

    update

---
+++ TravellerBob/TravellerBob.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class TravellerBob
+{
+    static void Main()
+    {
+        // inputs
+        string yearType = Console.ReadLine();
+        int contractMonths = int.Parse(Console.ReadLine());
+        int familyMonths = int.Parse(Console.ReadLine());
+        double regularMonths = 12D - contractMonths - familyMonths;
+
+        // logic
+        double contractTravels = contractMonths * 12D;
+        double familyTravels = familyMonths * 4D;
+        double regularTravels = regularMonths * (12D * 60D / 100D);
+        double totalTravels = contractTravels + familyTravels + regularTravels;
+
+        if (yearType == "leap")
+        {
+            totalTravels += totalTravels * 0.05;
+        }
+
+        // output
+        Console.WriteLine((int)totalTravels);
+    }
+}
