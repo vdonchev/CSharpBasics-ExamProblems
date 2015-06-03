d2c141739734c03f2fd4cd6ac6ca2c3081572a98
xclusivePc <xmt@abv.bg>
2015-06-03 23:35:30 +0300

    update

---
+++ CatchTheBits/CatchTheBits.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CatchTheBits
+{
+    class CatchTheBits
+    {
+        static void Main(string[] args)
+        {
+            int bits = int.Parse(Console.ReadLine());
+            int step = int.Parse(Console.ReadLine());
+
+            string output = String.Empty;
+            string array = String.Empty;
+
+            for (int i = 0; i < bits; i++)
+            {
+                var digit = int.Parse(Console.ReadLine());
+                array += Convert.ToString(digit, 2).PadLeft(8, '0');
+            }
+
+            for (int i = 1; i < array.Length; i += step)
+            {
+                output += array[i];
+                if (output.Length >= 8)
+                {
+                    Console.WriteLine(Convert.ToInt32(output, 2));
+                    output = String.Empty;
+                }
+            }
+
+            if (output.Length > 0)
+            {
+                while (output.Length != 8)
+                {
+                    output += 0;
+                }
+                Console.WriteLine(Convert.ToInt32(output, 2));
+            }
+        }
+    }
+}
