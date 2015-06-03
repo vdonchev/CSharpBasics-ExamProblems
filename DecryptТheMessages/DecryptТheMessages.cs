b541b1bd4cb83c5fee1c6b5264c4b8eef6e606bb
xclusivePc <xmt@abv.bg>
2015-06-03 15:52:22 +0300

    update

---
+++ DecryptТheMessages/DecryptТheMessages.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+public class DecryptТheMessages
+{
+    static void Main()
+    {
+        string message = Console.ReadLine();
+
+        int messages = 0;
+        string messageTemp = "";
+        string messageDecrypted = "";
+
+        while (message != null && message.ToLower() != "start")
+        {
+            message = Console.ReadLine();
+        }
+
+        message = "";
+
+        while (message.ToLower() != "end")
+        {
+            if (message != "" && message.ToLower() != "end" && !string.IsNullOrWhiteSpace(message))
+            {
+                for (int i = 0; i < message.Length; i++)
+                {
+                    if (Char.IsLetter(message[i]))
+                    {
+                        if (message[i] >= 'a' && message[i] < 'n' || message[i] >= 'A' && message[i] < 'N')
+                        {
+                            messageTemp += (char)(message[i] + 13);
+                        }
+                        else if (message[i] >= 'n' && message[i] <= 'z' || message[i] >= 'N' && message[i] <= 'Z')
+                        {
+                            messageTemp += (char)(message[i] - 13);
+                        }
+                    }
+                    else if (Char.IsDigit(message[i]))
+                    {
+                        messageTemp += message[i];
+                    }
+                    else
+                    {
+                        switch (message[i])
+                        {
+                            case '+':
+                                messageTemp += ' ';
+                                break;
+                            case '%':
+                                messageTemp += ',';
+                                break;
+                            case '&':
+                                messageTemp += '.';
+                                break;
+                            case '#':
+                                messageTemp += '?';
+                                break;
+                            case '$':
+                                messageTemp += '!';
+                                break;
+                            default:
+                                messageTemp += message[i]; // it should not but anyway...
+                                break;
+                        }
+                    }
+                }
+
+                char[] temp = messageTemp.ToCharArray();
+                Array.Reverse(temp);
+                messageDecrypted += new string(temp);
+                messageDecrypted += Environment.NewLine;
+                messages++;
+                messageTemp = "";
+            }
+
+            message = Console.ReadLine();
+        }
+        if (messages > 0)
+        {
+            Console.WriteLine("Total number of messages: {0}", messages);
+            Console.WriteLine(messageDecrypted);
+        }
+        else
+        {
+            Console.WriteLine("No message received.");
+        }
+    }
+}
---
+++ RockLq/RockLq.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class RockLq
+{
+    static void Main()
+    {
+        int input = int.Parse(Console.ReadLine());
+        int outter = input - 2;
+        int middle = 1;
+        int inner = input + 2;
+
+        Console.WriteLine("{0}{1}{0}", new string('.', input), new string('*', input));
+
+        for (int i = 0; i < input / 2; i++)
+        {
+            Console.WriteLine("{0}*{1}*{0}", new string('.', outter), new string('.', inner));
+            outter -= 2;
+            inner += 4;
+        }
+
+        Console.WriteLine("*{0}*{1}*{0}*", new string('.', input - 2), new string('.', input));
+
+        outter = input - 4;
+        for (int i = 1; i < input / 2; i++)
+        {
+            Console.WriteLine("*{0}*{2}*{1}*{2}*{0}*", new string('.',outter), new string('.', input), new string('.', middle));
+            middle += 2;
+            outter -= 2;
+        }
+
+        outter = input - 1;
+        inner = input;
+        char innerChar = '.';
+
+        for (int i = 0; i < input; i++)
+        {
+            innerChar = (i == input - 1) ? '*' : innerChar;
+            Console.WriteLine("{0}*{1}*{0}", new string('.', outter), new string(innerChar, inner));
+            inner += 2;
+            outter--;
+        }
+    }
+}
