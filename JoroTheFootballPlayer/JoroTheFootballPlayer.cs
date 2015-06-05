737bc3ec7b7ff09a0370fe640bfd2bc8f651b6bc
xclusivePc <xmt@abv.bg>
2015-06-05 08:52:35 +0300

    update

---
+++ JoroTheFootballPlayer/JoroTheFootballPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class JoroTheFootballPlayer
+{
+    static void Main(string[] args)
+    {
+        string year = Console.ReadLine();
+        int hollyday = int.Parse(Console.ReadLine());
+        int homeTown = int.Parse(Console.ReadLine());
+
+        int allWeeks = 52 - homeTown;
+        double totalPlays = ((allWeeks * 2d) / 3d) + (hollyday / 2d) + homeTown;
+        totalPlays = (year != "f") ? totalPlays + 3 : totalPlays;
+
+        Console.WriteLine((int)totalPlays);
+    }
+}
---
+++ Sunglasses/Sunglasses.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Sunglasses
+{
+    static void Main()
+    {
+        int height = int.Parse(Console.ReadLine());
+        int width = height * 2;
+
+        for (int i = 0; i < height; i++)
+        {
+            if (i == 0 || i == height - 1)
+            {
+                TopBottom(height);
+            }
+            else
+            {
+                Middle(height, i);
+            }
+        }
+    }
+
+    private static void TopBottom(int n)
+    {
+        string asterix = new string('*', n * 2);
+        string bridge = new string(' ', n);
+        Console.WriteLine("{0}{1}{0}", asterix, bridge);
+    }
+
+    private static void Middle(int n, int i)
+    {
+        string asterix = new string('*', 1);
+        string fill = new string('/', n * 2 - 2);
+        string bridge = new string(' ', n);
+        if (i == n / 2)
+        {
+            bridge = new string('|', n);
+        }
+
+        Console.WriteLine("{0}{1}{0}{2}{0}{1}{0}", asterix, fill, bridge);
+    }
+}
