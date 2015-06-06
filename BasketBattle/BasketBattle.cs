68c861a79aba516201abc18500cb626cf5132ee3
xclusivePc <xmt@abv.bg>
2015-06-06 14:52:11 +0300

    update

--- BasketBattle/BasketBattle.cs
+++ BasketBattle/BasketBattle.cs
@@ -1,91 +1,88 @@
 ﻿using System;
 
-namespace BasketBattle
+class BasketBattle
 {
-    class BasketBattle
+    private static void Main()
     {
-        private static void Main()
+        string homePlayer = Console.ReadLine();
+        string awayPlayer = (homePlayer == "Simeon") ? "Nakov" : "Simeon";
+        int rounds = int.Parse(Console.ReadLine());
+
+        int homeScore = 0;
+        int awayScore = 0;
+
+        for (int i = 0; i < rounds; i++)
         {
-            string homePlayer = Console.ReadLine();
-            string awayPlayer = (homePlayer == "Simeon") ? "Nakov" : "Simeon";
-            int rounds = int.Parse(Console.ReadLine());
-
-            int homeScore = 0;
-            int awayScore = 0;
-
-            for (int i = 0; i < rounds; i++)
+            if (i % 2 == 0)
             {
-                if (i % 2 == 0)
+                for (int j = 0; j < 2; j++)
                 {
-                    for (int j = 0; j < 2; j++)
+                    int scoreTry = int.Parse(Console.ReadLine());
+                    string scoreResult = Console.ReadLine();
+
+                    if (scoreResult == "success")
                     {
-                        int scoreTry = int.Parse(Console.ReadLine());
-                        string scoreResult = Console.ReadLine();
+                        homeScore += (j == 0) ? (homeScore + scoreTry <= 500) ? scoreTry : 0 : 0;
+                        awayScore += (j == 1) ? (awayScore + scoreTry <= 500) ? scoreTry : 0 : 0;
+                    }
 
-                        if (scoreResult == "success")
-                        {
-                            homeScore += (j == 0) ? (homeScore + scoreTry <= 500) ? scoreTry : 0 : 0;
-                            awayScore += (j == 1) ? (awayScore + scoreTry <= 500) ? scoreTry : 0 : 0;
-                        }
-
-                        if (homeScore > awayScore && homeScore == 500)
-                        {
-                            Console.WriteLine(homePlayer);
-                            Console.WriteLine(i + 1);
-                            Console.WriteLine(awayScore);
-                            return;
-                        }
-                        else if (homeScore < awayScore && awayScore == 500)
-                        {
-                            Console.WriteLine(awayPlayer);
-                            Console.WriteLine(i + 1);
-                            Console.WriteLine(homeScore);
-                            return;
-                        }
+                    if (homeScore > awayScore && homeScore == 500)
+                    {
+                        Console.WriteLine(homePlayer);
+                        Console.WriteLine(i + 1);
+                        Console.WriteLine(awayScore);
+                        return;
+                    }
+                    else if (homeScore < awayScore && awayScore == 500)
+                    {
+                        Console.WriteLine(awayPlayer);
+                        Console.WriteLine(i + 1);
+                        Console.WriteLine(homeScore);
+                        return;
                     }
                 }
-                else
-                {
-                    for (int j = 0; j < 2; j++)
-                    {
-                        int scoreTry = int.Parse(Console.ReadLine());
-                        string scoreResult = Console.ReadLine();
-
-                        if (scoreResult == "success")
-                        {
-                            homeScore += (j == 1) ? (homeScore + scoreTry <= 500) ? scoreTry : 0 : 0;
-                            awayScore += (j == 0) ? (awayScore + scoreTry <= 500) ? scoreTry : 0 : 0;
-                        }
-
-                        if (homeScore > awayScore && homeScore == 500)
-                        {
-                            Console.WriteLine(homePlayer);
-                            Console.WriteLine(i + 1);
-                            Console.WriteLine(awayScore);
-                            return;
-                        }
-                        else if (homeScore < awayScore && awayScore == 500)
-                        {
-                            Console.WriteLine(awayPlayer);
-                            Console.WriteLine(i + 1);
-                            Console.WriteLine(homeScore);
-                            return;
-                        }
-                    }
-                }
-            }
-
-            if (homeScore == awayScore)
-            {
-                Console.WriteLine("DRAW");
-                Console.WriteLine(homeScore);
             }
             else
             {
-                string winner = (homeScore > awayScore) ? homePlayer : awayPlayer;
-                Console.WriteLine(winner);
-                Console.WriteLine(Math.Abs(homeScore - awayScore));
+                for (int j = 0; j < 2; j++)
+                {
+                    int scoreTry = int.Parse(Console.ReadLine());
+                    string scoreResult = Console.ReadLine();
+
+                    if (scoreResult == "success")
+                    {
+                        homeScore += (j == 1) ? (homeScore + scoreTry <= 500) ? scoreTry : 0 : 0;
+                        awayScore += (j == 0) ? (awayScore + scoreTry <= 500) ? scoreTry : 0 : 0;
+                    }
+
+                    if (homeScore > awayScore && homeScore == 500)
+                    {
+                        Console.WriteLine(homePlayer);
+                        Console.WriteLine(i + 1);
+                        Console.WriteLine(awayScore);
+                        return;
+                    }
+                    else if (homeScore < awayScore && awayScore == 500)
+                    {
+                        Console.WriteLine(awayPlayer);
+                        Console.WriteLine(i + 1);
+                        Console.WriteLine(homeScore);
+                        return;
+                    }
+                }
             }
+        }
+
+        if (homeScore == awayScore)
+        {
+            Console.WriteLine("DRAW");
+            Console.WriteLine(homeScore);
+        }
+        else
+        {
+            string winner = (homeScore > awayScore) ? homePlayer : awayPlayer;
+            Console.WriteLine(winner);
+            Console.WriteLine(Math.Abs(homeScore - awayScore));
         }
     }
 }
--- BasketBattle/Properties/AssemblyInfo.cs
+++ BasketBattle/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("BasketBattle")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("BasketBattle")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("409cb57e-676c-41da-a359-f76288dd917c")]
+[assembly: Guid("6589d821-f87d-4471-a6ee-43e3292fe3a7")]
 
 // Version information for an assembly consists of the following four values:
 //
