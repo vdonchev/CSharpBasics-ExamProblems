5f68ecca188acc98afc78e5b0ae0ca830efd5d01
xclusivePc <xmt@abv.bg>
2015-06-06 14:48:43 +0300

    update

---
+++ BiggestTriple/BiggestTriple.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class BiggestTriple
+{
+    static void Main()
+    {
+        string[] numbers = Console.ReadLine().Split(' ');
+        int biggestSum = Int32.MinValue;
+        int count = 0;
+        string winningTripple = string.Empty;
+
+        string currentTriple = string.Empty;
+        int currentSum = 0;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            currentSum += int.Parse(numbers[i]);
+            currentTriple += numbers[i] + ' ';
+            count++;
+            if (count >= 3 || i == numbers.Length - 1)
+            {
+                if (currentSum > biggestSum)
+                {
+                    biggestSum = currentSum;
+                    winningTripple = currentTriple;
+                }
+
+                currentSum = 0;
+                currentTriple = string.Empty;
+                count = 0;
+            }
+
+        }
+        Console.WriteLine(winningTripple);
+    }
+}
---
+++ SpyHard/SpyHard.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SpyHard
+{
+    class SpyHard
+    {
+        static void Main()
+        {
+            int numSystem = int.Parse(Console.ReadLine());
+            string message = Console.ReadLine();
+
+            string output = string.Empty;
+            int messageCrypt = 0;
+            char messageChar;
+
+            output += numSystem;
+            output += message.Length;
+
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                if (char.IsLetter(message[i]))
+                {
+                    messageChar = char.ToLower(message[i]);
+                    messageCrypt += (messageChar - 96);
+                }
+                else
+                {
+                    messageCrypt += (int) message[i];
+                }
+            }
+            output += Convert.ToString(messageCrypt, numSystem);
+            Console.WriteLine(output);
+        }
+    }
+}
