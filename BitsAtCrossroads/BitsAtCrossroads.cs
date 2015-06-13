900eaef88405c4d5009d5788fcd9e727091c2888
xclusivePc <xmt@abv.bg>
2015-06-14 00:21:22 +0300

    update

---
+++ BitsAtCrossroads/BitsAtCrossroads.cs
@@ -0,0 +1,137 @@
+﻿using System;
+
+class BitsAtCrossroads
+{
+    static void Main()
+    {
+        // get the board size
+        int size = int.Parse(Console.ReadLine());
+
+        // fill the board
+        int[] nums = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            nums[i] = 0;
+        }
+
+        int crossroad = 0;
+
+        // get the 1st command
+        string[] action = Console.ReadLine().Split();
+
+        // loop through the commands
+        while (action[0] != "end")
+        {
+            int y = int.Parse(action[0]);
+            int x = int.Parse(action[1]);
+
+            // set the current crossroad
+            nums[y] |= 1 << x;
+            crossroad++;
+
+            // print left-top
+            bool ok = true;
+            int currentY = y - 1;
+            int currentX = x + 1;
+            while (ok)
+            {
+                if (currentY >= 0 && currentX <= (size - 1))
+                {
+                    if ((nums[currentY] >> currentX & 1) == 1)
+                    {
+                        crossroad++;
+                    }
+                    nums[currentY] |= 1 << currentX;
+                    currentY--;
+                    currentX++;
+                }
+                else
+                {
+                    ok = false;
+                }
+            }
+
+            // print top-right
+            ok = true;
+            currentY = y - 1;
+            currentX = x - 1;
+            while (ok)
+            {
+                if (currentY >= 0 && currentX >= 0)
+                {
+                    if ((nums[currentY] >> currentX & 1) == 1)
+                    {
+                        crossroad++;
+                    }
+                    nums[currentY] |= 1 << currentX;
+                    currentY--;
+                    currentX--;
+                }
+                else
+                {
+                    ok = false;
+                }
+            }
+
+            // print bottom-right
+            ok = true;
+            currentY = y + 1;
+            currentX = x - 1;
+            while (ok)
+            {
+                if (currentY <= (size - 1) && currentX >= 0)
+                {
+                    if ((nums[currentY] >> currentX & 1) == 1)
+                    {
+                        crossroad++;
+                    }
+                    nums[currentY] |= 1 << currentX;
+                    currentY++;
+                    currentX--;
+                }
+                else
+                {
+                    ok = false;
+                }
+            }
+
+            // print bottom-left
+            ok = true;
+            currentY = y + 1;
+            currentX = x + 1;
+            while (ok)
+            {
+                if (currentY <= (size - 1) && currentX <= (size - 1))
+                {
+                    if ((nums[currentY] >> currentX & 1) == 1)
+                    {
+                        crossroad++;
+                    }
+                    nums[currentY] |= 1 << currentX;
+                    currentY++;
+                    currentX++;
+                }
+                else
+                {
+                    ok = false;
+                }
+            }
+
+            // debug
+            //foreach (var num in nums)
+            //{
+            //    Console.WriteLine(Convert.ToString(num, 2).PadLeft(size, '0'));
+            //}
+
+            // get another command
+            action = Console.ReadLine().Split();
+        }
+
+        // output
+        foreach (var num in nums)
+        {
+            Console.WriteLine((uint)num);
+        }
+        Console.WriteLine(crossroad);
+    }
+}
---
+++ HalfByteSwapper/HalfByteSwapper.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+class HalfByteSwapper
+{
+    static void Main()
+    {
+        uint[] nums = new uint[4];
+
+        for (int i = 0; i < 4; i++)
+        {
+            nums[i] = uint.Parse(Console.ReadLine());
+        }
+
+        string[] command1 = Console.ReadLine().Split();
+        if (command1[0] == "End")
+        {
+            foreach (var num in nums)
+            {
+                Console.WriteLine(num);
+            }
+            return;
+        }
+
+        string[] command2 = Console.ReadLine().Split();
+
+
+        while (command1[0] != "End")
+        {
+            int num1 = int.Parse(command1[0]);
+            int group1 = int.Parse(command1[1]) * 4;
+            int num2 = int.Parse(command2[0]);
+            int group2 = int.Parse(command2[1]) * 4;
+
+            for (int i = 0; i < 4; i++)
+            {
+                uint bit = nums[num1] >> (group1 + i) & 1;
+                uint bit2 = nums[num2] >> (group2 + i) & 1;
+
+                if (bit == 0)
+                {
+                    nums[num2] &= ~(1u << (group2 + i));
+                }
+                else
+                {
+                    nums[num2] |= 1u << (group2 + i);
+                }
+
+                if (bit2 == 0)
+                {
+                    nums[num1] &= ~(1u << (group1 + i));
+                }
+                else
+                {
+                    nums[num1] |= 1u << (group1 + i);
+                }
+            }
+
+            command1 = Console.ReadLine().Split();
+            if (command1[0] == "End")
+            {
+                break;
+            }
+            command2 = Console.ReadLine().Split();
+        }
+
+        foreach (var num in nums)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
