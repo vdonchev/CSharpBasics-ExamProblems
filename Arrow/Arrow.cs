13f8f670a2227216bd0dd21974a873c72967cb93
vdonchev <ya.5164646777752015@yandex.ru>
2015-06-18 11:31:05 +0300

    update

---
+++ Arrow/Arrow.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Arrow
+{
+    static void Main()
+    {
+        // input
+        int width = int.Parse(Console.ReadLine());
+        int heigth = width - 2;
+        char arrow = '#';
+        char bg = '.';
+
+        // print first line
+        Console.WriteLine("{0}{1}{0}", new string(bg, width / 2), new string(arrow, width));
+
+        // print tail
+        for (int i = 0; i < heigth; i++)
+        {
+            Console.WriteLine("{0}{2}{1}{2}{0}", new string(bg, width / 2), new string(bg, width - 2), arrow);
+        }
+
+        // print top
+        Console.WriteLine("{0}{1}{0}", new string(arrow, (width / 2) + 1), new string(bg, width - 2));
+
+        int outter = 1;
+        int inner = (width * 2) - 5;
+
+        for (int i = 0; i < heigth; i++)
+        {
+            Console.WriteLine("{0}{2}{1}{2}{0}", new string(bg, outter), new string(bg, inner), arrow);
+            outter++;
+            inner -= 2;
+        }
+
+        Console.WriteLine("{0}{1}{0}", new string(bg, width - 1), arrow);
+    }
+}
---
+++ FiveSpecialLetters/FiveSpecialLetters.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+
+class FiveSpecialLetters
+{
+    static void Main()
+    {
+        int start = int.Parse(Console.ReadLine());
+        int stop = int.Parse(Console.ReadLine());
+        int match = 0;
+
+        string[] letters = { "a", "b", "c", "d", "e" };
+
+        string word = String.Empty;
+        for (int i = 0; i < 5; i++)
+        {
+            string first = letters[i];
+            for (int j = 0; j < 5; j++)
+            {
+                string second = letters[j];
+                for (int k = 0; k < 5; k++)
+                {
+                    string third = letters[k];
+                    for (int l = 0; l < 5; l++)
+                    {
+                        string fourth = letters[l];
+                        for (int m = 0; m < 5; m++)
+                        {
+                            string fifth = letters[m];
+                            word = first + second + third + fourth + fifth;
+                            int wordWeigth = CalcWord(word);
+
+                            if (wordWeigth >= start && wordWeigth <= stop)
+                            {
+                                Console.Write(word + " ");
+                                match++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        if (match == 0)
+        {
+            Console.WriteLine("No");
+        }
+        else
+        {
+            Console.WriteLine();
+        }
+    }
+
+    private static int CalcWord(string word)
+    {
+        string clean = new string(word.ToCharArray().Distinct().ToArray());
+        int weigth = 0;
+        int index = 1;
+        for (int i = 0; i < clean.Length; i++)
+        {
+            weigth += index * CalcLetters(clean[i]);
+            index++;
+        }
+
+        return weigth;
+    }
+
+    private static int CalcLetters(char letter)
+    {
+        int letterWeigth = 0;
+        switch (letter)
+        {
+            case 'a':
+                letterWeigth = 5;
+                break;
+            case 'b':
+                letterWeigth = -12;
+                break;
+            case 'c':
+                letterWeigth = 47;
+                break;
+            case 'd':
+                letterWeigth = 7;
+                break;
+            case 'e':
+                letterWeigth = -32;
+                break;
+        }
+        return letterWeigth;
+    }
+}
